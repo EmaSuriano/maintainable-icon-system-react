73a7d7c2cacbd557bd70d8f976cdf4c88242b0ea
EmaSuriano <emanuel.suriano@example.com>
2019-09-14 01:10:01 +0200

    Updates

---
+++ precache-manifest.2f35ca024fd27d10c87a848048976c2c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99477bb7d40f56879cbbcf57f3624bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7035508876cef93cdd2e",
+    "url": "/static/js/2.e1c50e13.chunk.js"
+  },
+  {
+    "revision": "26f516e9638294d2d281",
+    "url": "/static/js/main.00448c6f.chunk.js"
+  },
+  {
+    "revision": "08308bbf1c805f3e6da4",
+    "url": "/static/js/runtime~main.1be9acbe.js"
+  },
+  {
+    "revision": "ce1e22b98ad7ff8443f96e42d2925408",
+    "url": "/static/media/account_balance.ce1e22b9.svg"
+  },
+  {
+    "revision": "81ed5cff012a0279a2baa763eadc5702",
+    "url": "/static/media/add_circle.81ed5cff.svg"
+  },
+  {
+    "revision": "92d6a119f97f1e116f691dbdd52c711e",
+    "url": "/static/media/alarm.92d6a119.svg"
+  },
+  {
+    "revision": "8c947471f7db87b1e0e2e02eedcb4c71",
+    "url": "/static/media/assistant.8c947471.svg"
+  },
+  {
+    "revision": "5d54888a969c666dedb7ac53d7df3dce",
+    "url": "/static/media/brightness.5d54888a.svg"
+  },
+  {
+    "revision": "bc683f5dface29baee96f55199a93ca0",
+    "url": "/static/media/calendar_today.bc683f5d.svg"
+  },
+  {
+    "revision": "5d5fb241dee6369dde61ec41dc8b38e8",
+    "url": "/static/media/camera.5d5fb241.svg"
+  },
+  {
+    "revision": "3bf58906f404a88230c3b3c0c6f57757",
+    "url": "/static/media/camera_enhance.3bf58906.svg"
+  },
+  {
+    "revision": "c80c037d18585c036640e9a3ce4e91c8",
+    "url": "/static/media/camera_plus.c80c037d.svg"
+  },
+  {
+    "revision": "0f35fb0406e230318978e1d419c0b960",
+    "url": "/static/media/cloud_verified.0f35fb04.svg"
+  },
+  {
+    "revision": "f9e85d70fd268cdd8fa10c15a88021a9",
+    "url": "/static/media/directions_boat.f9e85d70.svg"
+  },
+  {
+    "revision": "fedede26c47fe3f38efb0d7315f1c31a",
+    "url": "/static/media/directions_bus.fedede26.svg"
+  },
+  {
+    "revision": "3a0d4f26c4af4f199b166afd2625454f",
+    "url": "/static/media/directions_car.3a0d4f26.svg"
+  },
+  {
+    "revision": "a52cb8da98c9d952a7d98326c9f7b3bc",
+    "url": "/static/media/directions_subway.a52cb8da.svg"
+  },
+  {
+    "revision": "82b013e03d969d277bc3960e858b0b75",
+    "url": "/static/media/directions_train.82b013e0.svg"
+  },
+  {
+    "revision": "676fedf2a6e3309ac3c7e98bd05a2445",
+    "url": "/static/media/headset.676fedf2.svg"
+  },
+  {
+    "revision": "fd20d38d209e179aed76e0957e963108",
+    "url": "/static/media/home.fd20d38d.svg"
+  },
+  {
+    "revision": "eeb9fc11163e8210e64de1602177251a",
+    "url": "/static/media/offline_bolt.eeb9fc11.svg"
+  },
+  {
+    "revision": "b448e096c07174dd57774750920efbd7",
+    "url": "/static/media/star.b448e096.svg"
+  },
+  {
+    "revision": "76bf004a6bc122f4133415541aa78039",
+    "url": "/static/media/thumb_up.76bf004a.svg"
+  },
+  {
+    "revision": "ef34492dabd60210fbdda80237f0a255",
+    "url": "/static/media/verified_user.ef34492d.svg"
+  },
+  {
+    "revision": "63c563d140898db3c5e8385e16eef0ce",
+    "url": "/static/media/whatshot.63c563d1.svg"
+  }
+]);
